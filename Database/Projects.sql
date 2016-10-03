1be0d1af84a3936564cefff42fd1287ad9afa036
Petr Torbinskiy <p.torbinskiy@gmail.com>
2016-10-03 11:50:46 +0300

    Setup DB

---
+++ Database/Projects.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Projects]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(255) NOT NULL, 
+    [RootRevision] INT NOT NULL, 
+    [RowVersion] ROWVERSION NOT NULL
+)
