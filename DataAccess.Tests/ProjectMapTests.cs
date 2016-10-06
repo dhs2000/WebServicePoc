3604fadd8bfd86c664547d552d8d23ef6568787b
Petr Torbinskiy <p.torbinskiy@gmail.com>
2016-10-06 09:04:46 +0300

    Fix Test - Add Force Lock

--- DataAccess.Tests/ProjectMapTests.cs
+++ DataAccess.Tests/ProjectMapTests.cs
@@ -84,9 +84,11 @@ namespace DataAccess.Tests
                 using (ITransaction transaction = session.BeginTransaction())
                 {
                     var project = session.Get<Project>(id);
+                    session.Lock(project, LockMode.Force);
 
                     project.AddItem(Guid.NewGuid(), "Item3");
 
+                    project.Items[1].File.Name.Should().Be("File1");
                     project.Items.Count.Should().Be(3);
                     transaction.Commit();
                 }
