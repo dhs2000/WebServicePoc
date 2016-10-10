6a24d5f6647534e84ffba72a2bfe500f0e20d22e
Petr Torbinskiy <p.torbinskiy@gmail.com>
2016-10-10 18:05:45 +0300

    Fix stylecop

--- src/WebServicePoc/ProgramOptions.cs
+++ src/WebServicePoc/ProgramOptions.cs
@@ -1,21 +1,18 @@
 ﻿using CommandLine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebServicePoc
 {
     public class ProgramOptions
     {
-        [Option(DefaultValue = "self-host", HelpText = "Sets hosting desire - self-host or service-fabric-host.", Required = false)]
+        [Option(DefaultValue = "self-host", HelpText = "Sets hosting desire - self-host or service-fabric-host.",
+             Required = false)]
         public string Host { get; set; }
 
         [Option(DefaultValue = "http", HelpText = "The target protocol - Options [http] or [https]")]
         public string Protocol { get; set; }
 
         [Option(DefaultValue = "localhost", HelpText = "The target IP Address or Uri.")]
-        public string IpAddressOrFQDN { get; set; }
+        public string IpAddressOrFqdn { get; set; }
 
         [Option(DefaultValue = "8585", HelpText = "The target port where app will run")]
         public string Port { get; set; }
--- src/WebServicePoc/ServiceFabric/StatelessWebService.cs
+++ src/WebServicePoc/ServiceFabric/StatelessWebService.cs
@@ -1,73 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Fabric;
-using System.Fabric.Query;
-using System.Linq;
+using System.Fabric.Description;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Services.Communication.Runtime;
-using Microsoft.ServiceFabric.Services.Runtime;
+
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
-using WebServicePoc;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
 
 namespace WebServicePoc.ServiceFabric
 {
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
-    internal sealed class StatelessWebService : Microsoft.ServiceFabric.Services.Runtime.StatelessService, ICommunicationListener
+    internal sealed class StatelessWebService : Microsoft.ServiceFabric.Services.Runtime.StatelessService,
+        ICommunicationListener
     {
-        private readonly string _endpointName;
+        private readonly string endpointName;
 
-        private IWebHost _webHost;
+        private IWebHost webHost;
 
         public StatelessWebService(StatelessServiceContext serviceContext, string endpointName)
-                : base(serviceContext)
-            {
-            _endpointName = endpointName;
-        }
-
-        #region StatelessService
-
-        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+            : base(serviceContext)
         {
-            return new[] { new ServiceInstanceListener(_ => this) };
+            this.endpointName = endpointName;
         }
-
-        #endregion StatelessService
-
-        #region ICommunicationListener
 
         void ICommunicationListener.Abort()
         {
-            _webHost?.Dispose();
+            this.webHost?.Dispose();
         }
 
         Task ICommunicationListener.CloseAsync(CancellationToken cancellationToken)
         {
-            _webHost?.Dispose();
+            this.webHost?.Dispose();
 
             return Task.FromResult(true);
         }
 
         Task<string> ICommunicationListener.OpenAsync(CancellationToken cancellationToken)
         {
-            var endpoint = FabricRuntime.GetActivationContext().GetEndpoint(_endpointName);
+            EndpointResourceDescription endpoint = FabricRuntime.GetActivationContext().GetEndpoint(this.endpointName);
 
             string serverUrl = $"{endpoint.Protocol}://{FabricRuntime.GetNodeContext().IPAddressOrFQDN}:{endpoint.Port}";
 
-            _webHost = new WebHostBuilder().UseKestrel()
-                                           .UseContentRoot(Directory.GetCurrentDirectory())
-                                           .UseStartup<Startup>()
-                                           .UseUrls(serverUrl)
-                                           .Build();
+            this.webHost =
+                new WebHostBuilder().UseKestrel()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseStartup<Startup>()
+                    .UseUrls(serverUrl)
+                    .Build();
 
-            _webHost.Start();
+            this.webHost.Start();
 
             return Task.FromResult(serverUrl);
         }
 
-        #endregion ICommunicationListener
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[] { new ServiceInstanceListener(_ => this) };
+        }
     }
 }
