926ccc5b9c56db0d261981dff55e765347ad521a
testuzver <sleepwalkergt@gmail.com>
2016-10-10 13:17:14 +0300

    - Added service fabric project - Added commandlineparser nuget package to parse program args - Extended webservice with stateless fabric service - Modified program.cs to allow hybrid run of web service - via local kestrel and via local cluster

--- src/WebServicePoc/Program.cs
+++ src/WebServicePoc/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System.IO;
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Threading;
+using System.Diagnostics;
+using System;
+using System.Linq;
+using WebServicePoc.ServiceFabric;
 
 namespace WebServicePoc
 {
@@ -8,14 +14,35 @@ namespace WebServicePoc
     {
         public static void Main(string[] args)
         {
-            IWebHost host =
-                new WebHostBuilder().UseKestrel()
+            var argsParser = new CommandLine.Parser();
+            var cmdOptions = new ProgramOptions();
+            bool argsParseSucceeded = argsParser.ParseArguments(args, cmdOptions);
+            if (argsParseSucceeded && cmdOptions.Host == "service-fabric-host")
+            {
+                ServiceRuntime.RegisterServiceAsync("WebServicePocType", context => new StatelessWebService(context, "ServiceEndpoint")).GetAwaiter().GetResult();
+                Thread.Sleep(Timeout.Infinite);
+            }
+            else
+            {
+                IWebHost host = new WebHostBuilder().UseKestrel()
                     .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseIISIntegration()
                     .UseStartup<Startup>()
                     .Build();
 
-            host.Run();
+                host.Run();
+            }
         }
+
+        //public static void Main(string[] args)
+        //{
+        //    IWebHost host =
+        //        new WebHostBuilder().UseKestrel()
+        //            .UseContentRoot(Directory.GetCurrentDirectory())
+        //            .UseIISIntegration()
+        //            .UseStartup<Startup>()
+        //            .Build();
+
+        //    host.Run();
+        //}
     }
 }
