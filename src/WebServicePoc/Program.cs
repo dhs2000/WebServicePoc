03ba29f0c0e4820f9603c0dcf2b5cb6bff290df3
Petr Torbinskiy <p.torbinskiy@gmail.com>
2016-10-10 15:33:12 +0300

    add IISIntegration

--- src/WebServicePoc/Program.cs
+++ src/WebServicePoc/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System.IO;
+using System.Threading;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.ServiceFabric.Services.Runtime;
-using System.Threading;
-using System.Diagnostics;
-using System;
-using System.Linq;
+
 using WebServicePoc.ServiceFabric;
 
 namespace WebServicePoc
@@ -17,32 +15,24 @@ namespace WebServicePoc
             var argsParser = new CommandLine.Parser();
             var cmdOptions = new ProgramOptions();
             bool argsParseSucceeded = argsParser.ParseArguments(args, cmdOptions);
-            if (argsParseSucceeded && cmdOptions.Host == "service-fabric-host")
+            if (argsParseSucceeded && (cmdOptions.Host == "service-fabric-host"))
             {
-                ServiceRuntime.RegisterServiceAsync("WebServicePocType", context => new StatelessWebService(context, "ServiceEndpoint")).GetAwaiter().GetResult();
+                ServiceRuntime.RegisterServiceAsync(
+                    "WebServicePocType",
+                    context => new StatelessWebService(context, "ServiceEndpoint")).GetAwaiter().GetResult();
                 Thread.Sleep(Timeout.Infinite);
             }
             else
             {
-                IWebHost host = new WebHostBuilder().UseKestrel()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseStartup<Startup>()
-                    .Build();
+                IWebHost host =
+                    new WebHostBuilder().UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>()
+                        .Build();
 
                 host.Run();
             }
         }
-
-        //public static void Main(string[] args)
-        //{
-        //    IWebHost host =
-        //        new WebHostBuilder().UseKestrel()
-        //            .UseContentRoot(Directory.GetCurrentDirectory())
-        //            .UseIISIntegration()
-        //            .UseStartup<Startup>()
-        //            .Build();
-
-        //    host.Run();
-        //}
     }
 }
